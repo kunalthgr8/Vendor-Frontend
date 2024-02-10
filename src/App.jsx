32d2d049c3f10bdb5983393d177835ef5cb59b9a
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-02-10 15:57:50 +0530

    Sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
-import './App.css'
-import {AddProduct,Contact,Home,LoginSignup,Order,Products,Sidebar,Topbar,Update,User} from "./components/index"
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import {
+  AddProduct,
+  Contact,
+  Home,
+  LoginSignup,
+  Order,
+  Products,
+  Sidebar,
+  Topbar,
+  Update,
+  User,
+} from "./components/index";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [isLogined, setIsLogined] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <div className="flex App">
+      {isLogined ? (
+        <>
+          <div className={`${windowWidth >= 1300 ? "sideb" : "nosideb"}`}>
+            <Sidebar />
+          </div>
+          <div className={`${windowWidth >= 1300 ? "content" : "nocontent"}`}>
+            <Topbar username={`${isLogined ? "Kunal Singla" : "Sign In"}`} />
+            <Routes>
+              <Route path="addproduct" element={<AddProduct />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="/" element={<Home />} />
+              <Route path="orderhistory" element={<Order />} />
+              <Route path="productpage" element={<Products />} />
+              <Route path="updateproduct" element={<Update />} />
+              <Route path="user" element={<User />} />
+
+              {/* Add a default route for unmatched paths */}
+              <Route path="*" element={<Home />} />
+            </Routes>
+          </div>
+        </>
+      ) : (
+        <Routes>
+          <Route path="login" element={<LoginSignup />} />
+
+          {/* Add a default route for unmatched paths */}
+          <Route path="*" element={<Login />} />
+        </Routes>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Logo from "../../assets/Logo.svg";
+import { IoHome } from "react-icons/io5";
+import { AiFillMedicineBox } from "react-icons/ai";
+import { FaCartShopping } from "react-icons/fa6";
+import { TbDiscount2 } from "react-icons/tb";
+import { IoMdContacts } from "react-icons/io";
+import { FiLogOut } from "react-icons/fi";
+import { Link, NavLink } from "react-router-dom";
+import "./Sidebar.css";
 
-function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const SideBar = () => {
 
-export default Sidebar
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowWidth >= 1300 || windowWidth <= 761 ? (
+    <div className="SideBar sidebar bg-sidebar-color text-white md:h-screen  md:fixed md:top-0 left-0 p-4">
+      <div className="Logo-SideBar">
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="Image-SideBar" />
+        </Link>
+      </div>
+      <div className="Title-SideBar">
+        <h2 className="Heading-Sidebar">Aoushadhi</h2>
+      </div>
+      <div className="Items-SideBar">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} Items-SideBars`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/productpage"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} Items-SideBars`
+          }
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="/addproduct"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} Items-SideBars`
+          }
+        >
+          Add Product
+        </NavLink>
+        <NavLink
+          to="/orderhistory"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} Items-SideBars`
+          }
+        >
+          Order History
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} Items-SideBars`
+          }
+        >
+          Contact Us
+        </NavLink>
+        <span className="User-SideBar">Log Out</span>
+      </div>
+      <div className="Footer-SideBar">
+        <p>
+          Copyright Aquired By<p>@Aoushadhi</p>
+        </p>
+      </div>
+    </div>
+  ) : (
+    <div className="Logo-Sidebar SideBar sidebar bg-sidebar-color text-white h-screen  fixed top-0 left-0 ">
+      <div className="img-small">
+        <Link to="/">
+          <img src={Logo} alt="Aoushadhi" />
+        </Link>
+      </div>
+      <div className="detail-Logo">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} logo-SideBars`
+          }
+        >
+          <IoHome />
+        </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} logo-SideBars`
+          }
+        >
+          <AiFillMedicineBox />
+        </NavLink>
+        <NavLink
+          to="/cart"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} logo-SideBars`
+          }
+        >
+          <FaCartShopping />
+        </NavLink>
+
+        <NavLink
+          to="/offers"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} logo-SideBars`
+          }
+        >
+          <TbDiscount2 />
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            ` ${isActive ? "Items-Active" : ""} logo-SideBars`
+          }
+        >
+          <IoMdContacts />
+        </NavLink>
+      </div>
+
+      <div className="logout-Logo">
+        <FiLogOut />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
