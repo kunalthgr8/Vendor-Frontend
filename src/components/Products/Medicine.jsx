64dcf16c3c75983465fa66f520cbf2214df44787
Lalit Gour <lalitg@iitbhilai.ac.in>
2024-02-14 15:08:23 +0530

    Product Add Backend Integration

--- src/components/Products/Medicine.jsx
+++ src/components/Products/Medicine.jsx
@@ -4,99 +4,101 @@ import CartCard from "../CartCard/CartCard";
 import Product from "../../assets/Product.svg";
 import axios from "axios";
 
+const userId="1234"
 const Products = () => {
-  const vendor_Id = localStorage.getItem("user_id");
-  const [products, setProducts] = useState();
+  // const vendor_Id = localStorage.getItem("user_id");
+  const [products, setProducts] = useState([""]);
   useEffect(() => {
-    console.log("useEffect");
-    const getAllProducts = async (e) => {
-      const products = await axios.get(
-        "http://localhost:4000/get_all_products"
-      );
-      // console.log(products.data)
-      setProducts(products.data);
-    };
-
     getAllProducts();
   }, []);
-  const prods = [
-    {
-      images: [],
-      _id: "65b7c52d4de23857fda6f564",
-      productId: "12140970",
-      userId: "1234",
-      product_name: "hii",
-      brand: "vase",
-      price: "2000",
-      createdAt: "2024-01-29T15:33:01.709Z",
-      updatedAt: "2024-01-29T15:33:01.709Z",
-      __v: 0,
-    },
-    {
-      images: [],
-      _id: "65b7c54e4de23857fda6f566",
-      productId: "12140980",
-      userId: "1235",
-      product_name: "Vaseline",
-      brand: "vaseeee",
-      price: "150",
-      createdAt: "2024-01-29T15:33:34.029Z",
-      updatedAt: "2024-01-29T15:33:34.029Z",
-      __v: 0,
-    },
-    {
-      images: [],
-      _id: "65b7c55c4de23857fda6f568",
-      productId: "12140990",
-      userId: "12345",
-      product_name: "Vaseline",
-      brand: "vaseeee",
-      price: "1500",
-      createdAt: "2024-01-29T15:33:48.978Z",
-      updatedAt: "2024-01-29T15:33:48.978Z",
-      __v: 0,
-    },
-    {
-      images: [],
-      _id: "65b7c5664de23857fda6f56a",
-      productId: "12140910",
-      userId: "12",
-      product_name: "Vaseline",
-      brand: "vaseeee",
-      price: "1500",
-      createdAt: "2024-01-29T15:33:58.879Z",
-      updatedAt: "2024-01-29T15:33:58.879Z",
-      __v: 0,
-    },
-    {
-      images: [],
-      _id: "65b9d927b1c9961ef2d8a887",
-      productId: "567",
-      userId: "120",
-      product_name: "lalit",
-      brand: "BMW",
-      price: "1200000",
-      createdAt: "2024-01-31T05:22:47.178Z",
-      updatedAt: "2024-01-31T05:22:47.178Z",
-      __v: 0,
-    },
-  ];
+
+  async function getAllProducts(e) {
+    console.log("userId", userId);
+    const products = await axios.get("http://localhost:4000/get_products_by_vendor_Id", {
+      params: { userId: "1234" },
+    });
+    // const data = response.data;
+    console.log(products.data);
+    setProducts(products.data);
+  }
+  console.log(products);
+  // const prods = [
+  //   {
+  //     images: [],
+  //     _id: "65b7c52d4de23857fda6f564",
+  //     productId: "12140970",
+  //     userId: "1234",
+  //     product_name: "hii",
+  //     brand: "vase",
+  //     price: "2000",
+  //     createdAt: "2024-01-29T15:33:01.709Z",
+  //     updatedAt: "2024-01-29T15:33:01.709Z",
+  //     __v: 0,
+  //   },
+  //   {
+  //     images: [],
+  //     _id: "65b7c54e4de23857fda6f566",
+  //     productId: "12140980",
+  //     userId: "1235",
+  //     product_name: "Vaseline",
+  //     brand: "vaseeee",
+  //     price: "150",
+  //     createdAt: "2024-01-29T15:33:34.029Z",
+  //     updatedAt: "2024-01-29T15:33:34.029Z",
+  //     __v: 0,
+  //   },
+  //   {
+  //     images: [],
+  //     _id: "65b7c55c4de23857fda6f568",
+  //     productId: "12140990",
+  //     userId: "12345",
+  //     product_name: "Vaseline",
+  //     brand: "vaseeee",
+  //     price: "1500",
+  //     createdAt: "2024-01-29T15:33:48.978Z",
+  //     updatedAt: "2024-01-29T15:33:48.978Z",
+  //     __v: 0,
+  //   },
+  //   {
+  //     images: [],
+  //     _id: "65b7c5664de23857fda6f56a",
+  //     productId: "12140910",
+  //     userId: "12",
+  //     product_name: "Vaseline",
+  //     brand: "vaseeee",
+  //     price: "1500",
+  //     createdAt: "2024-01-29T15:33:58.879Z",
+  //     updatedAt: "2024-01-29T15:33:58.879Z",
+  //     __v: 0,
+  //   },
+  //   {
+  //     images: [],
+  //     _id: "65b9d927b1c9961ef2d8a887",
+  //     productId: "567",
+  //     userId: "120",
+  //     product_name: "lalit",
+  //     brand: "BMW",
+  //     price: "1200000",
+  //     createdAt: "2024-01-31T05:22:47.178Z",
+  //     updatedAt: "2024-01-31T05:22:47.178Z",
+  //     __v: 0,
+  //   },
+  // ];
   return (
     <div className="product-page">
       <div className="heading-product">
         <h1>Showing all your uploaded Items</h1>
       </div>
       <div className="product-cards">
-        {prods.map((item, index) => (
+        {products.map((item, index) => (
           <CartCard
-            user_Id={vendor_Id}
             key={index}
             index={index}
             productId={item.productId}
             userId={item.userId}
             img={Product}
             price={item.price}
-            name={item.item_name}
+            name={item.product_name}
             desc={item.discount}
             quant={10}
           />
